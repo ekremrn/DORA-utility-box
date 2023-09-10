c8647f0fa98e2741d769bf7ce943f526652f7e81
ekremrn <e.eren778@gmail.com>
2023-09-10 16:49:36 +0300

    extractor script

---
+++ extractor.py
@@ -0,0 +1,36 @@
+import os
+import json
+
+from tqdm import tqdm
+from dotenv import load_dotenv
+
+from argparse import ArgumentParser
+from pymongo.server_api import ServerApi
+from pymongo.mongo_client import MongoClient
+
+load_dotenv()
+
+DATABASE_URI = str(os.getenv("DATABASE_URI"))
+DATABASE_NAME = str(os.getenv("DATABASE_NAME"))
+DATABASE_PRODUCTS_COLLECTION = str(os.getenv("DATABASE_PRODUCTS_COLLECTION"))
+
+CLIENT = MongoClient(DATABASE_URI, server_api=ServerApi("1"))
+DB = CLIENT[DATABASE_NAME]
+PRODUCTS_COLLECTION = DB[DATABASE_PRODUCTS_COLLECTION]
+
+parser = ArgumentParser()
+parser.add_argument("-p", "--jsonpath", type=str, required=True)
+opt = parser.parse_args()
+
+DATA = json.load(open(opt.jsonpath))
+
+process = tqdm(
+    DATA, desc="Extracting...", ncols=100, colour="green"
+)
+for row in process:
+    if PRODUCTS_COLLECTION.find_one({"_id": row.get("_id")}):
+        continue
+    PRODUCTS_COLLECTION.insert_one(row)
+
+
+CLIENT.close()
